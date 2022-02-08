1f7f348aa1d6103778b4dc6e0d3faf999394638e
gunamkwon <rnska96@mju.ac.kr>
2022-02-09 00:23:29 +0900

    feat: BottomNavigationView 구성 완료

---
+++ CustomMusicPlayer/app/src/main/java/com/example/custommusicplayer/SearchActivity.java
@@ -0,0 +1,54 @@
+package com.example.custommusicplayer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private Context mContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = this;
+        bindViews();
+    }
+
+    private void bindViews() {
+        bottomNavigationView = findViewById(R.id.bottom_navigtaion);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+                switch (item.getItemId()){
+                    case R.id.action_search:
+                        intent = new Intent(mContext, SearchActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.current_playlist:
+                        intent = new Intent(mContext, CurrentPlaylistActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.user_playlist:
+                        intent = new Intent(mContext, MyPlayListActivity.class);
+                        startActivity(intent);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+
+}
